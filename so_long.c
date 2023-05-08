12319b8a886615c25351921781788e05ecae59cd
Hamza el Haia <81889344+MSSDOSS@users.noreply.github.com>
2023-05-08 22:27:40 +0100

    anass mse7 koulchi

--- so_long.c
+++ so_long.c
@@ -12,14 +12,34 @@
 
 #include "so_long.h"
 
-int	main(void)
+static int	check_av(char *av)
+{
+	int i;
+
+	i = 0;
+	if (!av)
+		return (0);
+	while (av[i])
+		i++;
+	i -= 4;
+	if (av[i] == '.' && av[i + 1] == 'b' && av[i + 2] == 'e' && av[i + 3] == 'r')
+		return (1);
+	return (0);
+}
+
+int	main(int ac, char **av)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 640, 480, "so_long");
-	// mlx_key_hook(vars.win, key_hook, &vars);
-	mlx_loop(mlx_ptr);
+	if (ac != 2)
+		return (0);
+	if (check_av(av[1]))
+	{
+		mlx_ptr = mlx_init();
+		win_ptr = mlx_new_window(mlx_ptr, 640, 480, "so_long");
+		// mlx_key_hook(vars.win, key_hook, &vars);
+		mlx_loop(mlx_ptr);
+	}
+	return (0);
 }
